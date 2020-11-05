b0bc518f5793c8591581f1fec432c81b94029b7d
Laura Parker <lauramparker78@gmail.com>
2020-11-05 12:52:44 -0500

    Adding working timer and start quiz functions

--- script.js
+++ script.js
@@ -3,17 +3,19 @@
     //player information
 
     //general navigation assignments
-    var startButton = document.querySelector ("#startButton");   //startbutton in html/css
+    var startButton = document.getElementById("startButton");   //startbutton in html/css
     var nextButton = document.querySelector ("#nextButton");   //nextbutton toggles? to next question
     var viewHighScores = document.querySelector ("#scoresButton");   //view highscores button
+     //graps div with welcome and start button
     
     //timer variables
-    var secondsRemaining = document.querySelector ("#timeLeft");   //displayed time remaining
-    var totalSeconds = 60;
+    var timerDiv = document.querySelector ("#timeLeft"); //div containing the timer
+    var secondsRemaining = document.querySelector ("#timer");   //displayed time remaining
+    var totalSeconds = 60; //global variable, changes in countdown
       
     
     //question & answer variables
-    var arrayQuestion: [] array of questions?;
+    //var arrayQuestion: [] array of questions?;
     //object? with quiz questions. Lists with quiz answers as buttons? This might need to be a dynamic element.
 
     var answerChoice1 = document.querySelector ("#choice1"); //First answer Choice
@@ -41,36 +43,76 @@
       
     
     
-    //FUNCTIONS
+//FUNCTIONS
     
-    // function startGame () { //function start game .. calls startTimer and startQuestions
+// function startGame () //function start game ... includes 1) countDown function (var) and 2) startQuestions functions
+       
+function startGame () {    
     
-   
-  //  function startTimer () { //start timer. calls setInterval function countdown here - how long is quiz?
-    
-    
-    //function countDown() {  //Countdown function. timer requires nested function. 
-        function countDown = setInterval (function() { 
-            if (totalSeconds <=0) {
-                clearInterval(totalSeconds=0) 
-            }
-                    totalSecondsDisplay.innerHTML = totalSeconds
-            totalSeconds -=1
-        }, 1000)//time left?
+    document.getElementById("startButton").style.display="none";   //makes start button disappear
+    document.getElementById("welcome").style.display="none";   //makes welcome message disappear
 
-        document.getElementById("timeLeft") //string? convert to number?
+    //1) var countDown, set Interval function() //Countdown function. calls setInterval function and then displays seconds remaining 
+
+    var countDown= setInterval(function () {  
+        totalSeconds--; //subtracts 1 from totalSeconds
+        timer.textContent = totalSeconds;
+
+        if(totalSeconds === 0) {
+            clearInterval(countDown);
+            endGame();
+        }
+
+    }, 1000);
+
+      
+    //document.getElementById("quizQuestion").style.display="none";
+}
+
+
+//2) function startQuestions () {  
     
-        
-    
-    function startQuestions () {  //loops over questions; dynamically changes text in the question & answer choices. Reassigns content of html #quizQuestion
-        
-        document.quizQuestion.textContent="Can you answer the first question?";
-    
-    
+function startQuestions() {  //loops over questions; dynamically changes text in the question & answer choices. Reassigns content of html #quizQuestion  
+    let questionEl= document.getElementById("quizQuestion");
+    questionEl.textContent="This is the first question";
+}
+
+
+
+
+// function endGame..calls 3) function storeScores and 4) displayEnd
+
+function endGame () {
+    alert="Oh no! Out of time, game over.";
+};   
+
+
+    //3) function storeScore
+     
+    function storeScores () {
+
+    };
+
+
+
+    //4) function displayEnd
+
+    function displayEnd () {
+        const notime= document.createElement("p"); //Revisit. this could be an end container like start container.
+        notime.textContent="Oh no! Out of time, game over.";
+        timeleft.appendChild(notime);
+        timer.textContent= 60;
+
+    };
 
     
     
-    //EVENTS
+
+
+
+
+
+//EVENTS
     
     //User enters name or player id
     //User stores score on local storage
@@ -78,7 +120,7 @@
 
     
     //User clicks start button
-    startButton.addEventListener("click", countDown); //starts game with countdown clock
+    //startButton.addEventListener("click", countDown); //starts game with countdown clock
         //start Container disappears after Start button clicked
         //question Container appears after Start button clicked
 
@@ -88,3 +130,6 @@
     //Incorrect questions subtract time from clock
     //When last question is answered, game is over
     //When clock hits 0, game is over
+
+startButton.addEventListener("click", startGame); 
+startButton.addEventListener("click", startQuestions);
