1097740fb14e6608c56a80a515ae2ec8d198972a
Laura Parker <lauramparker78@gmail.com>
2020-11-05 22:22:13 -0500

    Drafting question and answer arrays, objects, starter functions

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
     var startButton = document.getElementById("startButton");   //startbutton in html/css
     var nextButton = document.querySelector ("#nextButton");   //nextbutton toggles? to next question
     var viewHighScores = document.querySelector ("#scoresButton");   //view highscores button
-     //graps div with welcome and start button
+
     
     //timer variables
     var timerDiv = document.querySelector ("#timeLeft"); //div containing the timer
@@ -15,44 +15,45 @@
       
     
     //question & answer variables
-    //var arrayQuestion: [] array of questions?;
-    //object? with quiz questions. Lists with quiz answers as buttons? This might need to be a dynamic element.
+    var arrayQuestion = [
+        "Commonly used data types DO NOT include:",
+        "The condition in an if / else statement is enclosed within____.",
+        "Arrays in JavaScript can be used to store____",
+        "String values must be encolsed within____ when being assigned to variables.",
+        "A very useful tool used during development and debugging for printing content to the debugger is:",
+    ];
+    
+    //var question1Choices = ["strings","booleans","alerts","numbers"];
 
-    var answerChoice1 = document.querySelector ("#choice1"); //First answer Choice
-    var answerChoice2 = document.querySelector ("#choice2"); //Second answer Choice
-    var answerChoice3 = document.querySelector ("#choice3"); //Third answer Choice
-    var answerChoice4 = document.querySelector ("#choice4"); //Fourth answer Choice
-    
+   // var question2Choices = ["quotes","curly brackets","parenthesis","square brackets"];
+
+    //var question3Choices = ["numbers and strings","other arrays","booleans","all of the above"];
+
+    //var question4Choices = ["commas","curly brackets","quotes","parenthesis"];
+
+   // var question5Choices = ["JavaScript","terminal/bash","for loops","console.log"];
 
 
-
-        //Elements
-  
-    
-    //Wrapper for answer choices
-    //var choiceEl = document.createElement("div"); // will need to center the div choiceEl, text-align center to center elements in the div
-       // questionContainer.appendChild(choiceEl);
-    
-   // var choiceList = document.createElement("nl");
-    
-    //creating answer buttons 1-4
-    //var choiceOne = document.createElement("button"); 
-      //  choiceEl.appendChild(choiceOne); //appending answer choices to the choice div box
-        //choiceOne.textContent = "Choice A";
-    
-      
-    
+    var firstquestion= {
+        question: "Commonly used data types DO NOT include:",
+        choiceone: "strings",
+        choicetwo: "booleans",
+        choicethree: "alerts",
+        choicefour: "numbers",
+        correctchoice: "alerts",
+    };
     
 //FUNCTIONS
     
-// function startGame () //function start game ... includes 1) countDown function (var) and 2) startQuestions functions
+// function startGame () //function start game includes 1) countDown var and setInterval function 
        
 function startGame () {    
     
     document.getElementById("startButton").style.display="none";   //makes start button disappear
-    document.getElementById("welcome").style.display="none";   //makes welcome message disappear
+    document.getElementById("welcome").style.display="none";
+    document.getElementById("instructions").style.display="none";   //makes welcome message disappear
 
-    //1) var countDown, set Interval function() //Countdown function. calls setInterval function and then displays seconds remaining 
+    //var countDown, set Interval function() //Countdown function. calls setInterval function and then displays seconds remaining 
 
     var countDown= setInterval(function () {  
         totalSeconds--; //subtracts 1 from totalSeconds
@@ -64,18 +65,52 @@ function startGame () {
         }
 
     }, 1000);
-
-      
-    //document.getElementById("quizQuestion").style.display="none";
-}
+  
+};
 
 
-//2) function startQuestions () {  
+ 
+// Append choice buttons to document
+
+
+function makeChoiceButtons(choicesArray) {
+                
+    var choice1=document.createElement("button");
+       // document.getElementById("first").appendChild(choice1);
+        choice1.innerHTML=choicesArray[0];  //make questions object sets and combine all choice buttons, questions, answer choices into one function for setting the question?
+                
+                var choice2=document.createElement("button");
+                    //document.getElementById("second").appendChild(choice2);
+                    choice2.innerHTML=choicesArray[1];
+                
+                var choice3=document.createElement("button");
+                    //document.getElementById("third").appendChild(choice3);
+                    choice3.innerHTML=choicesArray[2];
+                
+                var choice4=document.createElement("button");
+                   // document.getElementById("fourth").appendChild(choice4);
+                    choice4.innerHTML=choicesArray[3];
+
+            };
+
+
+// Start displaying questions after Start Quiz is clicked
+
+function displayQuestion() {
+
+    var questionEl= document.getElementById("quizQuestion");
+      for (let i=0; i<arrayQuestion.length; i++) {
+           questionEl.textContent=arrayQuestion(0);
+        };
+
+};
+
+
+
+
+//function startQuestions () {  
     
-function startQuestions() {  //loops over questions; dynamically changes text in the question & answer choices. Reassigns content of html #quizQuestion  
-    let questionEl= document.getElementById("quizQuestion");
-    questionEl.textContent="This is the first question";
-}
+  
 
 
 
@@ -94,23 +129,8 @@ function endGame () {
     };
 
 
-
-    //4) function displayEnd
-
-    function displayEnd () {
-        const notime= document.createElement("p"); //Revisit. this could be an end container like start container.
-        notime.textContent="Oh no! Out of time, game over.";
-        timeleft.appendChild(notime);
-        timer.textContent= 60;
-
-    };
-
     
-    
-
-
-
-
+   
 
 //EVENTS
     
@@ -131,5 +151,20 @@ function endGame () {
     //When last question is answered, game is over
     //When clock hits 0, game is over
 
+
 startButton.addEventListener("click", startGame); 
-startButton.addEventListener("click", startQuestions);
+startButton.addEventListener("click", makeChoiceButtons);
+startButton.addEventListener("click", displayQuestion);
+
+
+
+function setQuestion() {
+
+    var questionEl= document.getElementById("quizQuestion");
+        questionEl.innerHTML=[firstquestion,question];
+
+
+};
+
+setQuestion();
+
