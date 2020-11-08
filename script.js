ee86e292121c966299cf905bb10019f41f4a8815
Laura Parker <lauramparker78@gmail.com>
2020-11-07 20:33:25 -0500

    Converting answer questions and choices to array, adding loop, editing functions

--- script.js
+++ script.js
@@ -24,52 +24,54 @@
     var choice4;
 
 
-    var firstQuestion= {
+var allQuestionSets = [
+
+    {
         question: "Commonly used data types DO NOT include:",
         choiceone: "strings",
         choicetwo: "booleans",
         choicethree: "alerts",
         choicefour: "numbers",
         correctchoice: "alerts",
-    };
+    },
 
-    var secondQuestion= {
+    {
         question:  "The condition in an if / else statement is enclosed within____.",
         choiceone: "quotes",
         choicetwo: "curly brackets",
         choicethree: "parenthesis",
         choicefour: "square brackets",
         correctchoice: "curly brackets",
-    };
+    },
 
-    var thirdQuestion= {
+    {
         question:  "Arrays in JavaScript can be used to store____",
         choiceone: "numbers and strings",
         choicetwo: "other arrays",
         choicethree: "booleans",
         choicefour: "all of the above",
         correctchoice: "all of the above",
-    };
+    },
 
-    var fourthQuestion= {
+    {
         question:  "String values must be enclosed within____ when being assigned to variables.",
         choiceone: "commas",
         choicetwo: "curly brackets",
         choicethree: "quotes",
         choicefour: "parenthesis",
         correctchoice: "parenthesis",
-    };
+    },
 
-    var fifthQuestion= {
+     {
         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
         choiceone: "JavaScript",
         choicetwo: "terminal/bash",
         choicethree: "for loops",
         choicefour: "console.log",
         correctchoice: "console.log",
-    };
+    },
 
-
+];
     
 //FUNCTIONS
     
@@ -99,15 +101,6 @@ function startGame () {
 };
 
 
-// Start displaying questions after Start Quiz is clicked
-
-function displayQuestion(questionObject) {
-    
-    var questionEl= document.getElementById("quizQuestion");
-           questionEl.textContent=questionObject.question;
-
-};
-
 
 // Display and append answer choice buttons
 
@@ -115,68 +108,61 @@ function makeChoiceButtons() { //use question objects to populate. var or let or
                 
     choice1=document.createElement("button");
         document.getElementById("first").appendChild(choice1);
-       // choice1.textContent=firstQuestion.choiceone;  
+     
                 
     choice2=document.createElement("button");
         document.getElementById("second").appendChild(choice2);
-      //  choice2.textContent=firstQuestion.choicetwo;
+     
                 
     choice3=document.createElement("button");
         document.getElementById("third").appendChild(choice3);
-      //  choice3.textContent=firstQuestion.choicethree;
+     
                 
     choice4=document.createElement("button");
         document.getElementById("fourth").appendChild(choice4);
-       // choice4.textContent=firstQuestion.choicefour;
 
-    displayQuestion(firstQuestion);
-    displayChoices(firstQuestion);
+
 
 };
 
-//displays the Answer Choices
 
-function displayChoices(questionObject) {  
+function runQuiz () {
 
-    choice1.textContent=questionObject.choiceone;  
+    for (let i=0; i<6; i++) {
+
+        var questionEl= document.getElementById("quizQuestion");
+
+                questionEl.textContent=allQuestionSets[i].question;
+
+                choice1.textContent=allQuestionSets[i].choiceone;  
+                    
+                choice2.textContent=allQuestionSets[i].choicetwo;
+                        
+                choice3.textContent=allQuestionSets[i].choicethree;
+                        
+                choice4.textContent=allQuestionSets[i].choicefour;
+
+              //  var rightAnswer= allQuestionSets[i].correctchoice;
+        
             
-    choice2.textContent=questionObject.choicetwo;
-            
-    choice3.textContent=questionObject.choicethree;
-            
-    choice4.textContent=questionObject.choicefour;
+               
 
-    //need to assign the data-attribute" correct to the correct answer choice
- 
-        if (choiceone === correctchoice) {
-            choiceone.setAttribute(data-correct, true)
-        }
-            else {
-                if (choicetwo === correctchoice) {
-                    choicetwo.setAttribute(data-correct, true)
-            }
-                else {
-                    if (choicethree === correctchoice) {
-                        choicethree.setAttribute(data-correct, true)
-                }
-                    else{
-                        choicefour.setAttribute(data-correct, true)
-                    };
+    };
 
-        };
-
-};
-
+}; //end of runQuiz
 
 //this function interprets the answer choice and sends to wrongAnswer or rightAnswer functions
 
-//function readAnswers(questionObject) { 
-  
-  //questionObject.getAttribute(data-correct).onclick=rightAnswer();
+function checkAnswers() { 
 
-  //questionObject.getAttribute(data-correct).onclick=rightAnswer();
+    while (totalSeconds > 0) {
 
-};
+        for (let i=0; i<6; i++) {
+
+        
+        if (correctchoice.onclick=rightAnswer()
+        var answerEl= document.querySelectorAll("buttons");
+
 
 
 // Alert wrong answer choice. Subtract time if wrong;
@@ -245,49 +231,7 @@ startButton.addEventListener("click", startGame);
 
 startButton.addEventListener("click", makeChoiceButtons); 
 
-//startButton.addEventListener("click", function() {displayQuestion(firstQuestion)});
+startButton.addEventListener("click", runQuiz); 
 
+//startButton.addEventListener("click", checkAnswers);
 
-//while (totalSeconds > 0) {
-
-  //  quizContainer.addEventListener("click", readAnswers);
-
-    //if (totalSeconds ===0) { //Or LESS than zero (since we can subtract 10)
-      //  endGame();
-    //};
-
-    
-
-//}
-
-    
-
-
-
-//startButton.addEventListener("click", displayQuestion,thirdQuestion);
-
-document.getElementById("quizContainer").addEventListener("click", displayChoices.call(secondQuestion));
-
-//document.getElementById("startButton").onclick = displayQuestion.call(firstQuestion); 
-//document.getElementById("startButton").onclick = displayChoices.call(firstQuestion);
-
-//answerList.addEventListener("click", displayQuestion.call(secondQuestion));
-
-//document.querySelectorAll("li").click(displayQuestion.call(secondQuestion)); //does this need the parent node?? query selector not working
-
-    
-
-//makeChoiceButtons.call(secondQuestion);
-//displayQuestion.call(secondQuestion);
-
-
-//makeChoiceButtons.call(thirdQuestion);
-//displayQuestion.call(thirdQuestion);
-
-
-//makeChoiceButtons.call(fourthQuestion);
-//displayQuestion.call(fourthQuestion);
-
-
-//makeChoiceButtons.call(fifthQuestion);
-//displayQuestion.call(fifthQuestion);
